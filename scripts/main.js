6f03ec378f75dfc047b7532af5f38b70594193b0
Qubis1539 <67687077+Qubis1539@users.noreply.github.com>
2023-09-25 07:40:18 -0700

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,15 @@
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function(e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
 $(document).ready(function() {
     $(".owl-carousel").owlCarousel({
         loop: true,
